d410328d0ae07e4069a75585271cffd15a04ac19
fabztech <fabztech@outlook.com>
2022-09-03 00:17:55 -0500

    clase 13 finalizada - addEventListener

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //Crear variables que representen a los selectores
 const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
-let pRes = document.querySelector("#resultado");
+const pRes = document.querySelector("#resultado");
 
-function onClickBtn() {
-	//console.log(Number(input1.value) + Number(input2.value));
-	res = Number(input1.value) + Number(input2.value);
+function onClickBtn(event) {
+	// console.log({ event });
+	// event.preventDefault();
+	const res = Number(input1.value) + Number(input2.value);
 	pRes.innerText = "El resultado es: " + res;
 }
 
-// btn.addEventListener("click", onClickBtn());
+form.addEventListener("click", onClickBtn);

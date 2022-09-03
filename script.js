5b14a55491d28d418f44e863392414e6e88800f3
fabztech <fabztech@outlook.com>
2022-09-02 23:38:38 -0500

    clase 12 completada - Eventos en JavaScript: interactuando con usuarios

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
 //Crear variables que representen a los selectores
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.getElementsByClassName("parrafito");
-let pid = document.getElementById("pid");
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+let pRes = document.querySelector("#resultado");
 
-console.log(input.value);
+function onClickBtn() {
+	//console.log(Number(input1.value) + Number(input2.value));
+	res = Number(input1.value) + Number(input2.value);
+	pRes.innerText = "El resultado es: " + res;
+}
 
-console.log({
-	h1,
-	p,
-	parrafito,
-	pid,
-	input,
-});
-
-h1.innerText = "Levi <br> Huskey";
-// h1.setAttribute('class', 'rojo');
-// console.log(h1.getAttribute('class'));
-
-h1.classList.add("rojo");
-h1.classList.remove("verde");
-//h1.classList.toggle('morado');
-//h1.classList.contains('verde');
-
-input.value = "123";
-
-const img_creada = document.createElement("img");
-
-img_creada.setAttribute(
-	"src",
-	"https://static.zerochan.net/Attack.on.Titan.full.1571644.jpg"
-);
-pid.innerText = "";
-pid.append(img_creada);
+// btn.addEventListener("click", onClickBtn());

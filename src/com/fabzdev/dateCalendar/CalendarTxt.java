eacde95c3b90fa74a310c3770229cbcfaf24d777
Fabio <fabioescobarardila@hotmail.com>
2023-08-31 14:44:20 -0500

    Trabajando con el bloc de notas: calendario Test2.0

--- src/com/fabzdev/dateCalendar/CalendarTxt.java
+++ src/com/fabzdev/dateCalendar/CalendarTxt.java
@@ -1,57 +1,90 @@
+package com.fabzdev.dateCalendar;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class CalendarTxt{
 
 	String calendar = "";
 
 	//Método para llenar arreglos que contienen la información del calendario.
 	public void llenarCalendario(){
-	Calendar cal = Calendar.getInstance();
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.MONTH, 0);
 		
-	int aDay = cal.get(Calendar.DAY_OF_MONTH);
-	int aMonth = cal.get(Calendar.MONTH + 1);
-	int aYear = cal.get(Calendar.YEAR);
-	int nDays = nDiasMes(aMonth);
-	int nCDays = 0;	
+		int aDay = cal.get(Calendar.DAY_OF_MONTH);
+		int aMonth = cal.get(Calendar.MONTH)+1;
+		int aYear = cal.get(Calendar.YEAR);
+		int nDays = diasMes(aMonth+1);
+		int nCDays = 0;	
 
-	String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", 				"Noviembre", "Diciembre");
+		String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", 				"Noviembre", "Diciembre"};
 	
-	String fBasica = "+-----+-----+-----+-----+-----+-----+-----+"; 
-	String fMes = meses[aMonth] + " " + aYear; 			
-	String fDias = "| Dom | Lun | Mar | Mie | Jue | Vie | Sab |";	
-	int acum=0;							
-	int acum2=0;							
+		String fBasica = "+------+------+------+------+------+------+------+"; 
+		String sFMes = meses[aMonth-1] + " " + aYear;
+		String fMes = "";
+		String fDias = "| Dom | Lun | Mar | Mie | Jue | Vie | Sab |";	
+		int acum=0;							
+		int acum2=0;							
 	
-	Calendar calDay1 = getInstance();
-	calDay1.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 1);
+		Calendar calDay1 = Calendar.getInstance();
+		calDay1.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 1);
 
-	
-	//Definiendo el tamaño del calendario
-	if (calDay1.get(Calendar.DAY_OF_WEEK) == 6 && diasMes() == 31 || 						 	calDay1.get(Calendar.DAY_OF_WEEK) == 7 && diasMes(aMonth+1)==30)
-		nCDays=42;
-	else 
-		nCDays=35;
-	
-	int[] days = new int[nDiasMes(aMonth);
-	int[] cdays = new int[nCDays];
-
-	 for (int i : days){
-   	     days[acum] = acum+1;
-   	     acum++;
-	}
-
-	 for (int c : cdays){
-   	     cdays[acum2] = ' ';
-   	     acum2++;
-	}
-
-
-	for(int i=0; i<nCDays; i++){
-		if(i!=0 && i%7 == 0)
-			calendar = calendar +"|\n";
-		calendar = calendar + "|  " + cdays[0] + "  ";
+		//Creando fMes definitiva
+		for(int i=0; i < fBasica.length(); i++){
+			if(i==0 || i==(fBasica.length()-1)){
+				fMes= fMes + "|";
+			}else if (i == (fBasica.length()/2 - sFMes.length()/2)){
+				fMes= fMes + sFMes;
+				i = i + (sFMes.length()-1);
+			} else {  
+				fMes= fMes + " ";
+			}
 		}
-		calendar = calendar + "|";
+	
+		//Definiendo el tamaño del calendario
+		if (calDay1.get(Calendar.DAY_OF_WEEK) == 6 && diasMes(aMonth+1) == 31 || calDay1.get(Calendar.DAY_OF_WEEK) == 7 && diasMes(aMonth+1)==30)
+			nCDays=42;
+		else 
+			nCDays=35;
+	
+		
+		String[] cdays = new String[nCDays];
+
+		/*
+		 for (int i : days){
+   		     days[acum] = acum+1;
+	   	     acum++;
+		}
+		*/
+
+		
+		int primerDia = calDay1.get(Calendar.DAY_OF_WEEK);
+
+		for (int j = 0; j < nCDays; j++){
+		
+			if(j > nDays+1 || j < primerDia -1)
+				cdays[j] = "  ";
+			else if ((j - primerDia + 1) < 9)
+				cdays[j] = "0" + (j - primerDia +2);
+	   	     	else
+				cdays[j] = "" + (j - primerDia +2);
+		}
+
+		
+		//Creando string del calendario
+		calendar = fBasica + "\n" + fMes + "\n" + fBasica+ "\n";
+		for(int i=0; i<nCDays; i++){
+			if(i!=0 && i%7 == 0)
+				calendar = calendar +"|\n";
+			calendar = calendar + "|  " + cdays[i] + "  ";
+			}
+		calendar = calendar + "|" + "\n" + fBasica;
+
+		System.out.print(calendar);
+	
 	}
-}
+
 
 	//Método para obtener el número de dias que tiene un mes
 	public static int diasMes(int mes){
@@ -68,18 +101,13 @@ public class CalendarTxt{
         	        break;
 	            case 4:
         	    case 6:
-   	         case 9:
+   	            case 9:
         	    case 11:
 	                numeroDias=30;
         	        break;
 	            case 2:
-	                Date anioActual=new Date();
-	                if(esBisiesto(1900 + anioActual.getYear())){
-        	            numeroDias=29;
-                	}else{
-	                    numeroDias=28;
-        	        }
-                	break;
+	                numeroDias=28;
+        	        break;
         	}
 
 	    return numeroDias;
@@ -89,13 +117,12 @@ public class CalendarTxt{
 	//Método main
 	public static void main(String[] args){
 	
-	CalendarTxt calTxt = new CalendarTxt();
-	
-	
+		CalendarTxt calTxt = new CalendarTxt();
+		calTxt.llenarCalendario();
 	
 	}
 
-
+}
 
 
 /*

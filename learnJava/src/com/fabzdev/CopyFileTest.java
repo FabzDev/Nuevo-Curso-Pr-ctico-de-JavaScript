2b1a8ce08a045d548b5d670adf145366ee25e12f
Fabio <fabioescobarardila@hotmail.com>
2023-09-06 23:43:58 -0500

    TheJavaPathV2: CopyFile con streams Terminado

--- learnJava/src/com/fabzdev/CopyFileTest.java
+++ learnJava/src/com/fabzdev/CopyFileTest.java
@@ -1,16 +1,32 @@
 
 package com.fabzdev;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class CopyFileTest {
     
-    public static void copyFile(){
-        InputStream in = new FileInputStream("C:/TheJAvaPathV2/fabio.png");
+    public static void copyFile(File src, File dest) throws IOException{
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dest);
+        int b = in.read();
+        while (b != -1){
+            out.write(b);
+            b = in.read();
+        }
+        in.close();
+        out.close();
+        
     }
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        File fileSrc = new File("C:/TheJavaPathV2/fabio.png");
+        File fileDest = new File("C:/TheJavaPathV2/fabio2.png");
+        copyFile(fileSrc, fileDest);
     }
 }

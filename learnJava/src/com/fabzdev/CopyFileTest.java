76cf38fd96896d58560969b2221966507d198738
Fabio <fabioescobarardila@hotmail.com>
2023-09-06 23:35:36 -0500

    TheJavaPathV2: CopyFile con streams

---
+++ learnJava/src/com/fabzdev/CopyFileTest.java
@@ -0,0 +1,16 @@
+
+package com.fabzdev;
+
+import java.io.InputStream;
+
+
+public class CopyFileTest {
+    
+    public static void copyFile(){
+        InputStream in = new FileInputStream("C:/TheJAvaPathV2/fabio.png");
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}

bf2962733765cc71055533b1a2f881c8774d6dcb
Fabio <fabioescobarardila@hotmail.com>
2023-09-13 17:25:41 -0500

    TheJavaPathV2: Collections: Properties

--- learnJava/src/com/fabzdev/TestCollections.java
+++ learnJava/src/com/fabzdev/TestCollections.java
@@ -1,6 +1,7 @@
 package com.fabzdev;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +18,8 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestCollections {
 
@@ -122,12 +126,107 @@ public class TestCollections {
 
     }
 
+    private static void mapTest() {
+        Map map = new HashMap();
+        map.put("uno", 1);
+        map.put("dos", 2);
+        map.put("tres", 3);
+        map.put("cuatro", 4);
+        map.put("cinco", 5);
+
+        for (Iterator it = map.keySet().iterator(); it.hasNext();) {
+            String n = (String) it.next();
+            System.out.println(n);
+        }
+
+        System.out.println();
+
+        for (Iterator it = map.entrySet().iterator(); it.hasNext();) {
+            Map.Entry n = (Map.Entry) it.next();
+            System.out.println("Llave: " + n.getKey() + ", Valor: " + n.getValue());
+        }
+
+        System.out.println();
+
+        for (Iterator it = map.keySet().iterator(); it.hasNext();) {
+            System.out.println(map.get(it.next()));
+        }
+
+    }
+
+    private static void linkedHashMapTest() {
+        Map map = new LinkedHashMap();
+        map.put("uno", 1);
+        map.put("dos", 2);
+        map.put("tres", 3);
+        map.put("cuatro", 4);
+        map.put("cinco", 5);
+
+        for (Iterator it = map.entrySet().iterator(); it.hasNext();) {
+            Map.Entry n = (Map.Entry) it.next();
+            System.out.println(n);
+        }
+    }
+    
+    private static void treeMapTest() {
+        Map map = new TreeMap();
+        map.put("uno", 1);
+        map.put("dos", 2);
+        map.put("tres", 3);
+        map.put("cuatro", 4);
+        map.put("cinco", 5);
+
+        for (Iterator it = map.entrySet().iterator(); it.hasNext();) {
+            Map.Entry n = (Map.Entry) it.next();
+            System.out.println(n);
+        }
+    }
+    
+    private static void propertiesTest() {
+        Properties p = new Properties();
+        p.setProperty("Mushoku", "Tensei");
+        p.setProperty("Rudeus", "Gryrad");
+        p.setProperty("Eris", "Boras Gryrat");
+        p.setProperty("Silphyeth", "Fitz");
+        
+        try(OutputStream o = new FileOutputStream("C:/TheJavaPathV2/prop.p")){
+        p.store(o, "El mundo de Mushoku Tensei");            
+        } catch (IOException ex) {
+            System.out.println("Error de escritura: " + ex);
+        }
+        
+        try(InputStream o = new FileInputStream("C:/TheJavaPathV2/prop.p")){
+        p.load(o);
+        } catch (IOException ex) {
+            System.out.println("Error de Lectura: " + ex);
+        }
+        
+    }
+    
+     private static void propertiesTest2() {
+        Properties p = new Properties();
+        
+        try(InputStream in = new FileInputStream("C:/TheJavaPathV2/prop.p")){
+        p.load(in);
+            System.out.println(p.get("Eris"));    
+        
+        } catch (IOException ex) {
+            System.out.println("Error de Lectura: " + ex);
+        }
+        
+    }
+
     public static void main(String[] args) {
 //        testSet();
 //        equalsTest();
 //        iteratorTest();
 //        linkedHashSetTest();
-        treeSetTest();
+//        treeSetTest();
+//        mapTest();
+//        linkedHashMapTest();
+//        treeMapTest();
+//        propertiesTest();
+//        propertiesTest2();
 
     }
 }
---
+++ prop.p
@@ -0,0 +1,6 @@
+#El mundo de Mushoku Tensei
+#Wed Sep 13 17:23:26 COT 2023
+Eris=Boras Gryrat
+Mushoku=Tensei
+Rudeus=Gryrad
+Silphyeth=Fitz

25d011dc18422a0a4238d5f804b5b9da44464b40
Fabio <fabioescobarardila@hotmail.com>
2023-09-18 08:31:56 -0500

    TheJavaPathV2: Generics: SortDemo

--- learnJava/src/com/fabzdev/generics/sortDemo.java
+++ learnJava/src/com/fabzdev/generics/sortDemo.java
@@ -1,7 +1,9 @@
 package com.fabzdev.generics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  *
@@ -9,7 +11,7 @@ import java.util.Comparator;
  */
 public class sortDemo {
 
-    public static void main(String[] args) {
+    public static void sortDemo1() {
         String[] arr = {"x", "m", "c", "w", "a", "f", "d"};
 
         for (String s : arr) {
@@ -39,11 +41,44 @@ public class sortDemo {
                 }
                 return 0;
             }
-           
+
         });
         for (String s : arr) {
             System.out.print(s + ", ");
         }
         System.out.println();
+    }
+
+    public static void sortDemo2() {
+        List<String> arr2 = new ArrayList<>();
+        arr2.add("x");
+        arr2.add("m");
+        arr2.add("c");
+        arr2.add("w");
+        arr2.add("a");
+        arr2.add("f");
+        arr2.add("d");
+        arr2.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.compareTo(o2) > 0) {
+                    return -1;
+                } else if (o1.compareTo(o2) < 0) {
+                    return 1;
+                }
+                return 0;
+            }
+
+        });
+        for (String s : arr2) {
+            System.out.print(s + ", ");
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        sortDemo1();
+        sortDemo2();
     }
 }

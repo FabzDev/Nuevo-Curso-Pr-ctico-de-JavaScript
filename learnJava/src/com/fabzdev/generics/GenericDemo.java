5ee07ce90c789c5945ad83ffe893e4b6c8f34265
Fabio <fabioescobarardila@hotmail.com>
2023-09-21 22:26:09 -0500

    TheJavaPathV2: Generics: Genericos a detalle parte 1.3

---
+++ learnJava/src/com/fabzdev/generics/GenericDemo.java
@@ -0,0 +1,33 @@
+package com.fabzdev.generics;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class GenericDemo {
+
+    public static <T> List<T> setToList(Set<T> set, List<T> list) {
+        list.clear();
+        for (T setItem : set) {
+            list.add(setItem);
+        }
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Uno");
+        set.add("Dos");
+        set.add("Tres");
+        
+        List<String> list = new LinkedList<>();
+        
+        list = GenericDemo.<String>setToList(set, list);
+        
+        for(String n : list){
+            System.out.println(n);
+        }
+        
+    }
+}

65bf0128cdf2ba5f81be48ba53d3c7e69789e273
Fabio <fabioescobarardila@hotmail.com>
2023-09-22 01:01:28 -0500

    TheJavaPathV2: Generics: Genericos a detalle parte 4

---
+++ learnJava/src/com/fabzdev/generics/DestType.java
@@ -0,0 +1,26 @@
+package com.fabzdev.generics;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author fabio
+ */
+public class DestType {
+
+    public static <T> List<T> loadData() {
+        return new LinkedList<>();
+    }
+
+    public static void toLower(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, list.get(i).toLowerCase());
+        }
+    }
+
+    public static void main(String[] args) {
+//        List<String> list = DestType.<String>loadData();
+        toLower(loadData());
+    }
+}

77644f44933f53aba093a1018f5bb6ea90cf674c
Fabio <fabioescobarardila@hotmail.com>
2023-09-28 07:40:17 -0500

    TheJavaPathV2: DataStructure: Linked Lists v2

--- learnJava/src/com/fabzdev/generics/dataStructures/Employee.java
+++ learnJava/src/com/fabzdev/generics/dataStructures/Employee.java
@@ -6,13 +6,22 @@ package com.fabzdev.generics.dataStructures;
  * @author fabio
  */
 public class Employee {
-    private String name;
-    private String adress;
-    private boolean gender;
-    private double salary;
+    public String name;
+    public String adress;
+    public boolean gender;
+    public double salary;
+    public Employee supervisor;
     
     public void setName(String name){
      this.name = name;   
+    }
+    
+    public void setSupervisor(Employee supervisor){
+        this.supervisor = supervisor;
+    }
+    
+    public Employee getSupervisor(){
+        return this.supervisor;
     }
     
     public String getName(){
--- learnJava/src/com/fabzdev/generics/dataStructures/LinkedListDemo.java
+++ learnJava/src/com/fabzdev/generics/dataStructures/LinkedListDemo.java
@@ -30,7 +30,7 @@ public class LinkedListDemo {
         
         n=head;
         while(n != null){
-            n=n.getNext();
+            n=n.getNext(); 
         }
     }
 }

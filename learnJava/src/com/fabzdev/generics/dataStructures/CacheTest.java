a5881def1669e15347e470e8f50ebe1da5ca0bb6
Fabio <fabioescobarardila@hotmail.com>
2023-09-29 20:51:59 -0500

    TheJavaPathV2: DataStructure: Cache v3.0

--- learnJava/src/com/fabzdev/generics/dataStructures/CacheTest.java
+++ learnJava/src/com/fabzdev/generics/dataStructures/CacheTest.java
@@ -17,108 +17,104 @@ import java.util.StringTokenizer;
 public class CacheTest {
 
     class Cache {
+
         CacheNode head;
         int size = 0;
         int count = 0;
 
-        public CacheNode getNode(String word) {
-            if (head == null) {
-                return agregarNuevoNodo(word);
+        public Cache(int size){                                         
+            this.size = size;
+        }
+        
+        public SlowWord getSlowWord(String word){
+            if(nodoEncontrado(word)){
+                return reubicarNodoInicio(word);
             } else {
-                boolean encontrado = nodoEncontrado(word);
-                if (encontrado) {
-                    return reubicarNodoInicio(word);
-                } else {
-                    if (count > size + 1) {
-                        eliminarUltimoNodo();
-                    }
-                    return agregarNuevoNodo(word);
+                SlowWord nuevoSw = new SlowWord(word);
+                agregarNuevoNodo(new CacheNode(nuevoSw));
+                if(count>size)
+                    eliminarUltimoNodo();
+                return nuevoSw;
+            }
+        }
+
+        public void agregarNuevoNodo(CacheNode node) {
+            if(head == null){
+                head = node;
+                return;
+            }                
+            node.next = head;
+            head = node;
+            count++;
+        }
+
+        public SlowWord reubicarNodoInicio(String word) {
+            CacheNode prev = head;
+            CacheNode temp = head;
+            while (temp != null) {
+                if (temp.slowWord.word.equals(word)) {
+                    break;
                 }
+                prev = temp;
+                temp = temp.next;
+            }
+            prev.next = temp.next;
+            temp.next = head;
+            head = temp;
+            return head.slowWord;
+        }
+
+        public boolean nodoEncontrado(String word) {
+            CacheNode temp = head;
+            while (temp != null) {
+                if (temp.slowWord.word.equals(word)) {
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
+        }
+
+        public void eliminarUltimoNodo() {
+            CacheNode prev = head;
+            CacheNode temp = head;
+            while (temp.next != null) {
+                prev = temp;
+                temp = temp.next;
+            }
+            prev.next = null;
+            count--;
+        }
+
+    }
+
+    class CacheNode {
+        public SlowWord slowWord;
+        public CacheNode next;
+
+        public CacheNode(SlowWord slowWord) {
+            this.slowWord = slowWord;
+        }
+    }
+
+    class SlowWord {
+        String word;
+        
+        public SlowWord(String word) {
+            this.word = word;
+            try {
+                Thread.sleep(5);
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
-    
 
-    public CacheNode agregarNuevoNodo(String word) {
-        CacheNode nodo = new CacheNode(word);
-        nodo.next = head.next;
-        head = nodo;
-        count++;
-        return nodo;
-
-    }
-
-    public CacheNode reubicarNodoInicio(String word) {
-        CacheNode prev = head;
-        CacheNode temp = head;
-        while (temp != null) {
-            if (temp.word.equals(word)) {
-                break;
-            }
-            prev = temp;
-            temp = temp.next;
-        }
-        prev.next = temp.next;
-        temp.next = head;
-        head = temp;
-        return temp;
-    }
-
-    public boolean nodoEncontrado(String word) {
-        CacheNode temp = head;
-        boolean encontrado = false;
-
-        while (temp != null) {
-            if (temp.word.equals(word)) {
-                encontrado = true;
-                break;
-            }
-            temp = temp.next;
-        }
-        return encontrado;
-    }
-
-    public void eliminarUltimoNodo() {
-        CacheNode temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.prev.next = null;
-        count--;
-    }
-
-}
-
-class CacheNode {
-
-    public String word;
-    public CacheNode next;
-    public CacheNode prev;
-
-    public CacheNode(String word) {
-        this.word = word;
-    }
-}
-
-class SlowWord {
-
-    String word;
-
-    public SlowWord(String word) {
-        this.word = word;
-        try {
-            Thread.sleep(50);
-            System.out.println(word);
-        } catch (Exception e) {
-            System.out.println(e);
+        public String getWord() {
+            return word;
         }
     }
 
-    public String getWord() {
-        return word;
-    }
-}
-
-public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         new CacheTest().run();
     }
 
@@ -126,18 +122,18 @@ public static void main(String[] args) throws Exception {
         Set<String> wordList = new HashSet<>();
         int nWords = 0;
         int nLines = 1;
-        BufferedReader
-
-br = new BufferedReader(new InputStreamReader(CacheTest.class  
-
-.getResourceAsStream("/com/fabzdev/quijote.txt")));
+        BufferedReader br = new BufferedReader(new InputStreamReader(CacheTest.class
+                .getResourceAsStream("/com/fabzdev/quijote.txt")));
         String line = br.readLine();
+        
+        Cache cache = new Cache(128);
         while (line != null) {
             StringTokenizer st = new StringTokenizer(line, " ");
             while (st.hasMoreTokens()) {
                 String word = st.nextToken().trim();
-
-                SlowWord sw = new SlowWord(word);
+                
+                SlowWord sw = cache.getSlowWord(word);
+                System.out.println(sw.getWord() + " - cache: " + cache.count);
 
                 wordList.add(word);
                 nWords++;

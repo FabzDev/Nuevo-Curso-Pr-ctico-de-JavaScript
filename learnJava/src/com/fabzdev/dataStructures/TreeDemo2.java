7bb4a3ac74d07b025259f41dfdda999d1efe2c12
Fabio <fabioescobarardila@hotmail.com>
2023-10-02 23:05:28 -0500

    TheJavaPathV2: Recursividad

--- learnJava/src/com/fabzdev/dataStructures/TreeDemo2.java
+++ learnJava/src/com/fabzdev/dataStructures/TreeDemo2.java
@@ -11,17 +11,20 @@ public class TreeDemo2 {
         public String text;
         public TreeNode left;
         public TreeNode right;
-        
-        public TreeNode(){};
+
+        public TreeNode() {
+        }
+
+        ;
         
         public TreeNode(String text, TreeNode right, TreeNode left) {
             this.text = text;
             this.right = right;
             this.left = left;
         }
-        
-        public TreeNode createNode(){
-            
+
+        public TreeNode createNode() {
+
             TreeNode res = new TreeNode("Objeto", new TreeNode("Vivo",
                     new TreeNode("Vertebrado", new TreeNode("Caballo", null, null), new TreeNode("Perro", null, null)),
                     new TreeNode("Invertebrado", new TreeNode("Gusano", null, null), new TreeNode("Medusa", null, null))),
@@ -30,19 +33,23 @@ public class TreeDemo2 {
                             new TreeNode("Artificial", new TreeNode("Casa", null, null), new TreeNode("Bicicleta", null, null))));
             return res;
         }
- 
-//        public static void main(String[] args) {
-//            TreeDemo2 td2 = new TreeDemo2();
-//
-//            TreeDemo2.TreeNode res = td2.new TreeNode("Objeto", td2.new TreeNode("Vivo",
-//                    td2.new TreeNode("Vertebrado", td2.new TreeNode("Caballo", null, null), td2.new TreeNode("Perro", null, null)),
-//                    td2.new TreeNode("Invertebrado", td2.new TreeNode("Gusano", null, null), td2.new TreeNode("Medusa", null, null))),
-//                    td2.new TreeNode("No-Vivo",
-//                            td2.new TreeNode("Natural", td2.new TreeNode("Roca", null, null), td2.new TreeNode("Lago", null, null)),
-//                            td2.new TreeNode("Artificial", td2.new TreeNode("Casa", null, null), td2.new TreeNode("Bicicleta", null, null))));
-//
-//        }
-        
-        
+
+        public void printTree(TreeNode tree) {
+            if (tree == null) {
+                return;
+            }
+            printTree(tree.right);
+//            System.out.println(tree.text);
+            printTree(tree.left);
+            System.out.println(tree.text);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TreeDemo2 td2 = new TreeDemo2();
+        TreeNode tn = td2.new TreeNode();
+        TreeNode result = tn.createNode();
+        result.printTree(result);
     }
 }

014c6b94ec0ea06f567217fa9106fd865d403b45
Fabio <fabioescobarardila@hotmail.com>
2023-10-14 17:44:12 -0500

    TheJavaPathV2: AWT & Swing. BrickAnimation V1

--- learnJava/src/com/fabzdev/swing/awt/ImageDemo.java
+++ learnJava/src/com/fabzdev/swing/awt/ImageDemo.java
@@ -7,7 +7,6 @@ package com.fabzdev.Swing.awt;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import javax.imageio.ImageIO;

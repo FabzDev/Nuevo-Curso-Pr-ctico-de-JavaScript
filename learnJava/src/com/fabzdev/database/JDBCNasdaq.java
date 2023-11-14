f9403d657d2aab2b0925e5c1ae5f8880babd5d19
Fabio <fabioescobarardila@hotmail.com>
2023-11-14 18:59:42 -0500

    Reflexion

--- learnJava/src/com/fabzdev/database/JDBCNasdaq.java
+++ learnJava/src/com/fabzdev/database/JDBCNasdaq.java
@@ -28,7 +28,6 @@ public class JDBCNasdaq {
             con = DriverManager.getConnection("jdbc:derby://localhost:1527/c:/TheJavaPathV2/javaPathDB");
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println("Clase no encontrada: " + ex.getMessage());
-        
         }
         return con;
     }
@@ -37,67 +36,66 @@ public class JDBCNasdaq {
         Connection con = JDBCNasdaq.getConnection();
         con.setAutoCommit(false);
 
-        PreparedStatement ps = con.prepareStatement("insert into company (name, symbol) values (?, ?)");
+        PreparedStatement ps = con.prepareStatement("insert into company (name, symbol, id_companyStatus) values (?, ?, ?)");
         PreparedStatement ps2 = con.prepareStatement("select id_companyStatus from companyStatus where code=?");
         PreparedStatement ps3 = con.prepareStatement("insert into companystatus (code) values (?)", Statement.RETURN_GENERATED_KEYS);
-        PreparedStatement ps4 = con.prepareStatement("update companyStatus set id_companyStatus =?");
+        PreparedStatement ps4 = con.prepareStatement("update companyStatus set id_companyStatus =? where code =?");
 
-        
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(NasdaqReader2.getInputStream()));
         String line = br.readLine();
         line = br.readLine();
         String nextLine = br.readLine();
-        
+
         while (nextLine != null) {
             String symbol = null;
             String name = null;
             String companyStatus = null;
+            int idCompanyStatus = 1;
             int count2 = 1;
             StringTokenizer st = new StringTokenizer(line, "|");
             while (st.hasMoreElements()) {
-                if (count2== 1) {
+                if (count2 == 1) {
                     symbol = st.nextToken();
                 } else if (count2 == 2) {
                     name = st.nextToken();
                 } else if (count2 == 5) {
                     companyStatus = st.nextToken();
-                    
+
+                    //EMPIEZA
                     ps2.setString(1, companyStatus);
-                    ResultSet rs2 = ps2.executeQuery();
-//                    System.out.println(rs2.next());
-                    if(rs2.next()){
+                    ResultSet rsId = ps2.executeQuery();
+                    if (!rsId.next()) { // si no existe el registro, crealo
                         ps3.setString(1, companyStatus);
-                        int areKeys = ps3.executeUpdate();
-                        if(areKeys>0){
-                            ResultSet rsKeys = ps3.getGeneratedKeys();
-                            if(rsKeys.next()){
-                                int idCompanyStatus = rsKeys.getInt(1);
-                                ps4.setInt(1, idCompanyStatus);
-                            }
-                        }
+                        System.out.println(companyStatus);
+                        ps3.executeUpdate();
+                        ResultSet id = ps3.getGeneratedKeys();
+                        id.next();
+                        idCompanyStatus = id.getInt(1);
+                        ps4.setInt(1, idCompanyStatus);
+                        System.out.println(idCompanyStatus);
+                        ps4.setString(2, companyStatus);
+                        ps4.executeUpdate();
+                    } else {
+                        idCompanyStatus = rsId.getInt(1);
                     }
-//                    } else { //si si existe en la tabla
-                        //busca el primary key
-                        //agregalo a la tabla company
-//                    }
                     break;
+                } else {
+                    st.nextToken();
                 }
                 count2++;
             }
+//            System.out.println("Symbol: " + symbol + "// Name: " + name + " // ID Company Status " + idCompanyStatus);
 
             ps.setString(1, name);
             ps.setString(2, symbol);
+            ps.setInt(3, idCompanyStatus);
             ps.execute();
 
-            
-            System.out.println("Symbol: " + symbol + "// Name: " + name + " // Company Status " + companyStatus);
-            System.out.println();
-            
+//            System.out.println();
             line = nextLine;
             nextLine = br.readLine();
         }
-        
+
         ps.close();
         con.commit();
         con.close();
---
+++ learnJava/src/com/fabzdev/database/MethodList.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fabzdev.database;
+
+import java.lang.reflect.Method;
+
+/**
+ *
+ * @author fabio
+ */
+public class MethodList {
+    public static void main(String[] args) {
+        System.out.println("Metodos de la clase Integer");
+        Method listMethods[] = String.class.getMethods();
+        for(Method m : listMethods){
+            Class typeParam[] = m.getParameterTypes();
+            StringBuilder sb = new StringBuilder();
+            
+            for(Class c : typeParam){
+                if(sb.length() > 0)
+                    sb.append(", ");
+                
+                
+                if(c.isArray()){
+                    sb.append(c.getComponentType()).append("[]");
+                } else {
+                    sb.append(c.getName());
+                }
+            }
+            System.out.printf("%s(%s)\n", m.getName(), sb.toString());
+        }
+    }
+}

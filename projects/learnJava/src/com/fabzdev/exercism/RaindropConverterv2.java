9df197cbacada4a6be857d9287a7f48bf7fe64d5
Fabio <fabioescobarardila@hotmail.com>
2023-12-10 18:14:31 -0500

    Excersism: RaindropConverter V2

---
+++ projects/learnJava/src/com/fabzdev/exercism/RaindropConverterv2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fabzdev.exercism;
+
+/**
+ *
+ * @author fabio
+ */
+public class RaindropConverterv2 {
+
+    String convert(int number) {
+        StringBuilder temp = new StringBuilder();
+        if(number%3 == 0)
+            temp.append("Pling");
+        if(number%5 == 0)
+            temp.append("Plang");
+        if(number%7 == 0)
+            temp.append("Plong");
+        if(number%3 != 0 && number%5 != 0 && number%7 != 0)
+            temp.append(String.valueOf(number));
+        return temp.toString();
+    }
+    
+    public static void main(String[] args) {
+        RaindropConverterv2 rc = new RaindropConverterv2();
+        System.out.println(rc.convert(35));
+    }
+}

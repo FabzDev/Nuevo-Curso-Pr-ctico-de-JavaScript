f2bf3177840a73c82a083c741854baa2a2c17c5f
Fabio <fabioescobarardila@hotmail.com>
2023-12-13 18:27:12 -0500

    Exercism: CaptainsLog completed

--- learnJava/src/edu/fabzdev/exercism/Badge.java
+++ learnJava/src/edu/fabzdev/exercism/Badge.java
@@ -2,23 +2,14 @@ package edu.fabzdev.exercism;
 
 public class Badge {
     public String print(Integer id, String name, String department) {
-        String badge = null;
-        if(id == null){
-            if(department == null){
-                badge = String.format("%s - OWNER", name);
-            }else {
-                badge = String.format("%s - %S", name, department);
-            }
-        } else if(department == null){
-            badge = String.format("[%d] - %s - OWNER", id, name);
-        } else {
-            badge = String.format("[%d] - %s - %S", id, name, department);
-        }
-        return badge;
+        return String.format("%s%s%S",
+                id != null ? "[" + id + "] - " : "",
+                name,
+                department != null ? " - " + department.toUpperCase() : " - OWNER");
     }
 
     public static void main(String[] args) {
         Badge bg = new Badge();
-        System.out.println(bg.print(734, "Ernest Johnny Payne", "Strategic Communication"));
+        System.out.println(bg.print(874, "Ernest Johnny Payne", null));
     }
 }
